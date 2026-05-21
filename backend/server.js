a21d7fdc705739cc01577a2925a5ba7e4f114f4b
SanchezLuis296 <lf.sanchez24@info.uas.edu.mx>
2026-05-20 23:51:09 -0700

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,195 @@ app.post('/login', async (req, res) => {
 
 });
 
+// ===============================
+// OBTENER PRODUCTOS
+// ===============================
+
+app.get("/productos", async(req, res) => {
+
+    try {
+
+        const result = await sql.request().query(`
+            SELECT * FROM Productos
+            WHERE activo = 1
+        `);
+
+        res.json(result.recordset);
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({
+            message:"Error obteniendo productos"
+        });
+    }
+});
+
+
+// ===============================
+// AGREGAR PRODUCTO
+// ===============================
+
+app.post("/productos", async(req, res) => {
+
+    const { nombre, codigo, precio, stock, tipo_venta } = req.body;
+
+    try {
+
+        await sql.request()
+            .input("nombre", sql.VarChar, nombre)
+            .input("codigo", sql.VarChar, codigo)
+            .input("precio", sql.Decimal(10,2), precio)
+            .input("stock", sql.Decimal(10,2), stock)
+            .input("tipo_venta", sql.VarChar, tipo_venta)
+            .query(`
+                INSERT INTO Productos
+                (nombre, codigo, precio, stock, tipo_venta)
+                VALUES
+                (@nombre, @codigo, @precio, @stock, @tipo_venta)
+            `);
+
+        res.json({
+            success:true
+        });
+
+    } catch(error){
+
+        console.log(error);
+
+        res.status(500).json({
+            message:"Error agregando producto"
+        });
+    }
+});
+
+// ===============================
+// EDITAR PRODUCTO
+// ===============================
+
+app.put("/productos/:id", async(req, res) => {
+
+    const id = req.params.id;
+
+    const {
+        nombre,
+        codigo,
+        precio,
+        stock
+    } = req.body;
+
+    try {
+
+        await sql.request()
+            .input("id", sql.Int, id)
+            .input("nombre", sql.VarChar, nombre)
+            .input("codigo", sql.VarChar, codigo)
+            .input("precio", sql.Decimal(10,2), precio)
+            .input("stock", sql.Decimal(10,2), stock)
+
+            .query(`
+                UPDATE Productos
+                SET
+                    nombre = @nombre,
+                    codigo = @codigo,
+                    precio = @precio,
+                    stock = @stock
+                WHERE id_producto = @id
+            `);
+
+        res.json({
+            success:true
+        });
+
+    } catch(error){
+
+        console.log(error);
+
+        res.status(500).json({
+            message:"Error editando producto"
+        });
+    }
+});
+
+// ===============================
+// ELIMINAR PRODUCTO
+// ===============================
+
+app.delete("/productos/:id", async(req, res) => {
+
+    const id = req.params.id;
+
+    try {
+
+        await sql.request()
+            .input("id", sql.Int, id)
+
+            .query(`
+                UPDATE Productos
+                SET activo = 0
+                WHERE id_producto = @id
+            `);
+
+        res.json({
+            success:true
+        });
+
+    } catch(error){
+
+        console.log(error);
+
+        res.status(500).json({
+            message:"Error eliminando producto"
+        });
+    }
+});
+
+// ===============================
+// REGISTRAR VENTA
+// ===============================
+
+app.post("/ventas", async(req, res) => {
+
+    const { carrito } = req.body;
+
+    try {
+
+        for(const producto of carrito){
+
+            await sql.request()
+
+                .input(
+                    "cantidad",
+                    sql.Decimal(10,2),
+                    producto.cantidad
+                )
+
+                .input(
+                    "id",
+                    sql.Int,
+                    producto.id_producto
+                )
+
+                .query(`
+                    UPDATE Productos
+                    SET stock = stock - @cantidad
+                    WHERE id_producto = @id
+                `);
+        }
+
+        res.json({
+            success:true
+        });
+
+    } catch(error){
+
+        console.log(error);
+
+        res.status(500).json({
+            message:"Error venta"
+        });
+    }
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
